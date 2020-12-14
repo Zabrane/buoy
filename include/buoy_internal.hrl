c875aeb31be12f80e21c217f4ebbc14b1b0e9bff
Zabrane <zabrane3@gmail.com>
2020-12-14 01:56:34 +0100

    Update buoy_internal.hrl

--- include/buoy_internal.hrl
+++ include/buoy_internal.hrl
@@ -23,8 +23,16 @@
 -define(DEFAULT_RECONNECT_MAX, 120000).
 -define(DEFAULT_RECONNECT_MIN, 500).
 -define(DEFAULT_SOCKET_OPTIONS, [
+    inet,
     binary,
     {packet, raw},
-    {send_timeout, 1000},
-    {send_timeout_close, true}]).
--define(DEFAULT_TIMEOUT, 10000).
+    {header, 0},
+    {delay_send, false},
+    {nodelay, true},
+    {exit_on_close, true},
+    {show_econnreset, true},
+    {send_timeout_close, true},
+    %% {send_timeout, 1500},
+    {tos, 72},
+    {send_timeout_close, true} ]).
+-define(DEFAULT_TIMEOUT, 1000).
